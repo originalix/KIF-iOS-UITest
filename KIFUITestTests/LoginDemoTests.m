01830b3367b76da92ad33ea4d2fbcec5a9828fd3
Lix <xiao.liunit@gmail.com>
2017-05-09 15:52:23 +0800

    add delete cell tests function

--- KIFUITestTests/LoginDemoTests.m
+++ KIFUITestTests/LoginDemoTests.m
@@ -29,4 +29,24 @@
     [tester waitForViewWithAccessibilityLabel:AccessibilityConstants.mainView];
 }
 
+- (void)testLoginFailed {
+    [tester enterText:@"lix123" intoViewWithAccessibilityLabel:AccessibilityConstants.loginTextField];
+    [tester enterText:@"123456" intoViewWithAccessibilityLabel:AccessibilityConstants.passwordTextField];
+    [tester tapViewWithAccessibilityLabel:AccessibilityConstants.loginBtn];
+    [tester waitForViewWithAccessibilityLabel:@"登录失败"];
+}
+
+- (void)testMainTableView {
+    [self testLoginSuccess];
+    UITableView *tableView = (UITableView*)[tester waitForViewWithAccessibilityLabel:AccessibilityConstants.mainTableView];
+    NSInteger firstCount = [tableView numberOfRowsInSection:0];
+    [tester swipeRowAtIndexPath:[NSIndexPath indexPathForRow:5 inSection:0] inTableView:tableView inDirection:KIFSwipeDirectionLeft];
+    [tester tapViewWithAccessibilityLabel:@"删除"];
+    NSInteger lastCount = [tableView numberOfRowsInSection:0];
+    XCTAssertTrue(firstCount == lastCount + 1);
+    NSError *err = nil;
+    XCTAssertTrue([tester tryFindingViewWithAccessibilityLabel:@"第6行" error:&err], @"删除cell失败");
+}
+
+
 @end
