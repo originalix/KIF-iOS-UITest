4999b75024f8947e9403f7c734be3de6674221f5
Lix <xiao.liunit@gmail.com>
2017-05-09 12:15:45 +0800

    create login view

---
+++ KIFUITest/LoginViewController.m
@@ -0,0 +1,28 @@
+//
+//  LoginViewController.m
+//  KIFUITest
+//
+//  Created by Lix on 2017/5/9.
+//  Copyright © 2017年 Lix. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *loginTextField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+@property (weak, nonatomic) IBOutlet UIButton *loginBtn;
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
